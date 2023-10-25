4b82334a962407721c461b41d8d63700d52dd76e
Ian Manix <ian.manix@wsu.edu>
2023-10-25 12:37:40 -0700

    fixed bug with cave background despawning early

--- core/src/wsuv/bounce/PlayScreen.java
+++ core/src/wsuv/bounce/PlayScreen.java
@@ -845,7 +845,7 @@ public class PlayScreen extends ScreenAdapter {
             for (int j=7; j>0; j--) {
                 escapeGame.batch.draw(
                         cave_background.get(j),
-                        (start + i) * width,
+                        (start + i-1) * width,
                         -100,
                         width,
                         height
@@ -872,7 +872,7 @@ public class PlayScreen extends ScreenAdapter {
                 } else {*/
                     escapeGame.batch.draw(
                             surface_background.get(j),
-                            (start+i)*width,//*((-t)/((6-j)*5)),
+                            (start+i-1)*width,//*((-t)/((6-j)*5)),
                             height - 100,
                             width,
                             surface_background.get(0).getHeight() * surfaceBackgroundScale
@@ -893,7 +893,7 @@ public class PlayScreen extends ScreenAdapter {
             for (int j=0; j<3; j++) {
                 escapeGame.batch.draw(
                         skyFrame,
-                        (start+i)*width,
+                        (start+i-1)*width,
                         height - 100 + (frameHeight*j),
                         width,
                         frameHeight
